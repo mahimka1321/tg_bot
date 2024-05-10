d242bd3b71e35c8326ce3555fa5163e3cf0b3a25
mahimka1321git config --global user.name mahimka1321 <xakepok7771@mail.ru>
2024-05-11 01:10:09 +0300

    init-v-0.1.4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import { useEffect } from 'react';
+import {React, useEffect } from 'react';
 import './App.css';
 import { useTelegram } from './components/hooks/useTelegram';
+import Header from './components/header/Header';
 
 function App() {
   
@@ -17,6 +18,7 @@ function App() {
 
   return (
     <div className="App">
+      <Header/>
       <button onClick={onToggelButton}>
         toggle
       </button>
