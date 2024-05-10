bb9b87a71484ea31ea7c5b1160d94e3b0711347a
mahimka1321git config --global user.name mahimka1321 <xakepok7771@mail.ru>
2024-05-11 00:57:08 +0300

    init-v-0.1.2

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { useEffect } from 'react';
 import './App.css';
 
-
+const tg = window.Telegram.WebApp;
 function App() {
+  
+  useEffect(()=>{
+    tg.ready();
+  },[])
 
- 
+
 
   return (
     <div className="App">
-
     </div>
   );
 }
